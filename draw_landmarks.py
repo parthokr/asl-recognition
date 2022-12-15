68058a877d025802689b38ee53ba86c967192ec1
parthokr <partho.rajvor2016@gmail.com>
2022-12-15 15:13:05 +0600

    Draw styled landmarks on face, pose and hands

---
+++ draw_landmarks.py
@@ -0,0 +1,5 @@
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
